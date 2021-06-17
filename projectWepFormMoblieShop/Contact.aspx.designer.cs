9b70878e37c577b15296d1305b8d5198c344195b
Thin tran <trvathin@gmail.com>
2021-06-17 23:21:17 +0700

    Update

--- projectWepFormMoblieShop/Contact.aspx.designer.cs
+++ projectWepFormMoblieShop/Contact.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace projectWepFormMoblieShop
 
     public partial class Contact
     {
-
-        /// <summary>
-        /// formContact control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm formContact;
     }
 }
